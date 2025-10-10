e496623a821b6e6d38bf66129f94ee27d18d84c1
Igor Mogilnitskiy <igmo71@outlook.com>
2025-10-10 12:50:39 +0300

    XMS Aspire Project,

---
+++ XMS/XMS.AppHost/AppHost.cs
@@ -0,0 +1,16 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.XMS_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.XMS_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
