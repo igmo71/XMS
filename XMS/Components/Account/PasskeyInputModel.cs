cfa6dfa02faedf623c8a3a806a6318d56ac52fe9
Igor Mogilnitskiy <igmo71@outlook.com>
2026-01-27 17:37:20 +0300

    Add project files.

---
+++ XMS/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace XMS.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ XMS/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace XMS.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
