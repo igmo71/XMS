4f52c1ddfff25d00e718303f962e4dbbdaddd4df
Igor Mogilnitskiy <igmo71@outlook.com>
2026-02-10 11:24:29 +0300

    76384 ISoftDelete.

---
+++ XMS/Core/Abstractions/ISoftDelete.cs
@@ -0,0 +1,9 @@
+﻿namespace XMS.Core.Abstractions
+{
+    public interface ISoftDelete
+    {
+        bool IsDeleted { get; set; }
+        DateTimeOffset? DeletedAt { get; set; }
+        public Guid? DeletedBy { get; set; }
+    }
+}
--- XMS/Modules/Departments/Domain/Department.cs
+++ XMS/Modules/Departments/Domain/Department.cs
@@ -2,10 +2,14 @@
 
 namespace XMS.Modules.Departments.Domain
 {
-    public class Department : NamedEntity, ITreeNode<Department>
+    public class Department : NamedEntity, ITreeNode<Department>, ISoftDelete
     {
         public Guid? ParentId { get; set; }
         public Department? Parent { get; set; }
         public virtual ICollection<Department> Children { get; set; } = [];
+
+        public bool IsDeleted { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+        public Guid? DeletedBy { get; set; }
     }
 }
