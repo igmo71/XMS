85d5737b4e341032b50857fa5734c7ee6489cf86
Igor Mogilnitskiy <igmo71@outlook.com>
2026-02-19 16:41:57 +0300

    Add UT stock balances sync and UI grid

---
+++ XMS.Application/Abstractions/Services/IStockBalanceUtService.cs
@@ -0,0 +1,11 @@
+﻿using XMS.Domain.Models;
+
+namespace XMS.Application.Abstractions.Services
+{
+    public interface IStockBalanceUtService
+    {
+        Task<IReadOnlyList<StockBalanceUt>> GetListAsync(CancellationToken ct = default);
+        Task<IReadOnlyList<StockBalanceUt>> LoadListAsync(CancellationToken ct = default);
+        Task ReloadListAsync(CancellationToken ct = default);
+    }
+}
---
+++ XMS.Infrastructure/Data/EntityConfigurations/StockBalanceUtEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using XMS.Domain.Models;
+
+namespace XMS.Infrastructure.Data.EntityConfigurations
+{
+    public class StockBalanceUtEntityTypeConfiguration : BaseEntityTypeConfiguration<StockBalanceUt>
+    {
+        public override void Configure(EntityTypeBuilder<StockBalanceUt> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasIndex(x => new { x.NomenclatureId, x.WarehouseId }).IsUnique();
+            builder.Property(x => x.AvailableBalance).HasPrecision(18, 3);
+        }
+    }
+}
