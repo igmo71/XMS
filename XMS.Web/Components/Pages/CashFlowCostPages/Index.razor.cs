1e5b21011c8d54f182c6b69a20a692e8873c765a
Igor Mogilnitskiy <igmo71@outlook.com>
2026-03-10 18:05:11 +0300

    76578-2 CashFlowCostLookup. ProgressLinear.

--- XMS.Web/Components/Pages/CashFlowCostPages/Index.razor.cs
+++ XMS.Web/Components/Pages/CashFlowCostPages/Index.razor.cs
@@ -17,7 +17,7 @@ namespace XMS.Web.Components.Pages.CashFlowCostPages
         private readonly CancellationTokenSource _cts = new();
         private IReadOnlyList<CostCategory> _costCategories = [];
         private IReadOnlyList<CostCategoryItem> _costCategoryItems = [];
-        private IReadOnlyList<CashFlowCost> _cashFlowCosts = [];
+        private ILookup<Guid, CashFlowCost> _cashFlowCostLookup = default!;
         private IReadOnlyList<CashFlowItem> _cashFlowItems = [];
         private IReadOnlyList<TreeItemData<CashFlowItem>> _cashFlowItemsTree = [];
 
@@ -61,7 +61,11 @@ namespace XMS.Web.Components.Pages.CashFlowCostPages
         private async Task LoadCostCategories() => _costCategories = await CategoryService.GetListAsync(_includeDeleted, _cts.Token);
         private async Task LoadCostCategoryItems() => _costCategoryItems = await CategodyItemService.GetListAsync(_cts.Token);
         private async Task LoadCashFlowItems() => _cashFlowItems = await CashFlowItemService.GetListAsync(includeDeleted: false, _cts.Token);
-        private async Task LoadCashFlowCosts() => _cashFlowCosts = await CashFlowCostService.GetListAsync(_cts.Token);
+        private async Task LoadCashFlowCosts()
+        {
+            IReadOnlyList<CashFlowCost> cashFlowCostList = await CashFlowCostService.GetListAsync(_cts.Token);
+            _cashFlowCostLookup = cashFlowCostList.ToLookup(e => e.CostCategoryItemId);
+        }
 
         private void BuildCashFlowItemsTree()
         {
