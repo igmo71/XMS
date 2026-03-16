64d34fc92bc0410ccbc797ef878f99421d652821
Igor Mogilnitskiy <igmo71@outlook.com>
2026-03-16 14:32:16 +0300

    76649 Refactor EmployeeEndpints.

--- XMS.Application/Endpoints/EmployeeEndpoints.cs
+++ XMS.Application/Endpoints/EmployeeEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using XMS.Application.Abstractions.Services;
@@ -15,14 +16,16 @@ namespace XMS.Application.Endpoints
         {
             var employeeGroup = routeBuilder.MapGroup("/api/employees")
                 .WithTags("XMS Employees")
-                .AddEndpointFilter<ApiKeyAuthFilter>();
+                .AddEndpointFilter<ApiKeyAuthFilter>()
+                .ProducesProblem(401)
+                .ProducesValidationProblem();
 
             employeeGroup.MapGet("/", GetEmployeeList).WithName(nameof(GetEmployeeList));
             employeeGroup.MapGet("/{id}", GetEmployeeById).WithName(nameof(GetEmployeeById));
             employeeGroup.MapGet("/by-ut-refkey/{refKey}", GetEmployeeByUtRefKey).WithName(nameof(GetEmployeeByUtRefKey));
             employeeGroup.MapGet("/by-ad-login/{login}", GetEmployeeByAdLogin).WithName(nameof(GetEmployeeByAdLogin));
             //employeeGroup.MapGet("/by-manager-id/{id}", GetEmployeesByManagerId).WithName(nameof(GetEmployeesByManagerId));
-            employeeGroup.MapGet("/managers-by-employee-id/{id}",GetManagersByEmployeeId).WithName(nameof(GetManagersByEmployeeId));
+            employeeGroup.MapGet("/managers-by-employee-id/{id}", GetManagersByEmployeeId).WithName(nameof(GetManagersByEmployeeId));
 
             return routeBuilder;
         }
@@ -34,36 +37,38 @@ namespace XMS.Application.Endpoints
             [FromQuery] bool? includeDeleted)
         {
             var clientName = httpContext.Items["ClientName"];
+
             var result = await service.GetListAsync(new QueryParameters(skip, take, includeDeleted));
             return TypedResults.Ok(result);
         }
 
-        static async Task<IResult> GetEmployeeById(HttpContext httpContext,
+        static async Task<Results<Ok<Employee>, NotFound<Guid>>> GetEmployeeById(HttpContext httpContext,
             [FromServices] IEmployeeService service,
             [FromRoute] Guid id)
         {
             var clientName = httpContext.Items["ClientName"];
+
             var result = await service.GetByIdAsync(id);
-            return TypedResults.Ok(result);
+            return result is null ? TypedResults.NotFound(id) : TypedResults.Ok(result);
         }
 
-        static async Task<IResult> GetEmployeeByUtRefKey(HttpContext httpContext,
+        static async Task<Results<Ok<EmployeeDto>, NotFound<string>>> GetEmployeeByUtRefKey(HttpContext httpContext,
             [FromServices] IEmployeeService service,
-            [FromRoute] string refKey)
+            [FromRoute] string utRefKey)
         {
-            var employee = await service.GetByUtRefKeyAsync(refKey);
-            return employee is null ? TypedResults.NotFound() : TypedResults.Ok(EmployeeDto.From(employee));
+            var employee = await service.GetByUtRefKeyAsync(utRefKey);
+            return employee is null ? TypedResults.NotFound($"No Employee fount by Ut User Ref_Key: {utRefKey}") : TypedResults.Ok(EmployeeDto.From(employee));
         }
 
-        static async Task<IResult> GetEmployeeByAdLogin(HttpContext httpContext,
+        static async Task<Results<Ok<EmployeeDto>, NotFound<string>>> GetEmployeeByAdLogin(HttpContext httpContext,
             [FromServices] IEmployeeService service,
             [FromRoute] string login)
         {
             var employee = await service.GetByAdLoginAsync(login);
-            return employee is null ? TypedResults.NotFound() : TypedResults.Ok(EmployeeDto.From(employee));
+            return employee is null ? TypedResults.NotFound($"No Employee fount by AD User Login: {login}") : TypedResults.Ok(EmployeeDto.From(employee));
         }
 
-        static async Task<IResult> GetManagersByEmployeeId(HttpContext httpContext,
+        static async Task<Ok<IEnumerable<Guid>>> GetManagersByEmployeeId(HttpContext httpContext,
             [FromServices] IEmployeeService service,
             [FromRoute] Guid id)
         {
@@ -72,7 +77,7 @@ namespace XMS.Application.Endpoints
             return TypedResults.Ok(managers.Select(e => e.Id));
         }
 
-        static async Task<IResult> GetEmployeesByManagerId(HttpContext httpContext,
+        static async Task<Ok<IEnumerable<Guid>>> GetEmployeesByManagerId(HttpContext httpContext,
             [FromServices] IEmployeeService service,
             [FromRoute] Guid id)
         {
