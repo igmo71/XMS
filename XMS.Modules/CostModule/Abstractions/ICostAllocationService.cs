3bdfc203d7a1cc7fbb5599b76e22f4f15e29107f
Igor Mogilnitskiy <igmo71@outlook.com>
2026-04-09 16:04:12 +0300

    76849 Add CostAllocation service and page.

---
+++ XMS.Modules/CostModule/Abstractions/ICostAllocationService.cs
@@ -0,0 +1,5 @@
+﻿namespace XMS.Modules.CostModule.Abstractions;
+
+public interface ICostAllocationService
+{
+}
---
+++ XMS.Modules/CostModule/Application/CostAllocationService.cs
@@ -0,0 +1,7 @@
+﻿using XMS.Modules.CostModule.Abstractions;
+
+namespace XMS.Modules.CostModule.Application;
+
+internal class CostAllocationService : ICostAllocationService
+{
+}
