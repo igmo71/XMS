aee75d93388559f864070889438309f1eabc6e3d
Igor Mogilnitskiy <igmo71@outlook.com>
2026-04-14 11:08:04 +0300

    Add migration Catalog_КСЗ_КатегорииЗатрат.

---
+++ XMS.Infrastructure/Data/Migrations/20260414080150_AddКСЗ_КатегорияЗатрат_Key.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XMS.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddКСЗ_КатегорияЗатрат_Key : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "КСЗ_КатегорияЗатрат_Key",
+                table: "1c_ut_Document_СписаниеБезналичныхДенежныхСредств",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "КСЗ_КатегорияЗатрат_Key",
+                table: "1c_ut_Document_РасходныйКассовыйОрдер",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "КСЗ_КатегорияЗатрат_Key",
+                table: "1c_ut_Document_СписаниеБезналичныхДенежныхСредств");
+
+            migrationBuilder.DropColumn(
+                name: "КСЗ_КатегорияЗатрат_Key",
+                table: "1c_ut_Document_РасходныйКассовыйОрдер");
+        }
+    }
+}
