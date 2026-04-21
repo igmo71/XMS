afb60f619edf92b6238f8e3160c3ae4c1e2d8f81
Igor Mogilnitskiy <igmo71@outlook.com>
2026-04-21 18:07:34 +0300

    76971 Refactor.

--- XMS.Web/Components/Pages/CostSection/Catalog_СтатьиДДС_Pages/Index.razor.cs
+++ XMS.Web/Components/Pages/CostSection/Catalog_СтатьиДДС_Pages/Index.razor.cs
@@ -6,7 +6,7 @@ using XMS.Application.Integration.OneC.Common;
 using XMS.Application.Integration.OneC.Ut.Features.Catalog_СтатьиДвиженияДенежныхСредств_Feature;
 using XMS.Web.Components.Common;
 
-namespace XMS.Web.Components.Pages.CostSection.Catalog_СтатьиДвиженияДенежныхСредств_Pages;
+namespace XMS.Web.Components.Pages.CostSection.Catalog_СтатьиДДС_Pages;
 
 public partial class Index
 {
